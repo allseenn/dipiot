7e85f1cfdc0d58631e8062bd4ec9804915611e5f
allseenn <allseen@live.ru>
2024-07-05 01:49:55 +0300

    sense

--- web/web.c
+++ web/web.c
@@ -18,13 +18,23 @@ typedef struct {
     struct sockaddr_in client_addr;
 } client_info;
 
-
 bool check_auth(const char *auth_header) {
     char expected_auth[256];
     snprintf(expected_auth, sizeof(expected_auth), "Authorization: Basic %s", PASSWORD);
-    if(strcmp(auth_header, expected_auth) == 208)
-        return 0;
-    return 1;
+    if (strcmp(auth_header, expected_auth) == 0) {
+        return true;
+    }
+    return false;
+}
+
+void execute_command(const char *command, char *result, size_t result_size) {
+    FILE *fp = popen(command, "r");
+    if (fp == NULL) {
+        perror("popen");
+        exit(EXIT_FAILURE);
+    }
+    fgets(result, result_size, fp);
+    pclose(fp);
 }
 
 void *handle_client(void *arg) {
@@ -32,8 +42,10 @@ void *handle_client(void *arg) {
     char buffer[BUF_SIZE];
     int client_socket = client->client_socket;
     
-    FILE *fp;
-    char results[1024];
+    char temp_result[256] = "";
+    char pressure_result[256] = "";
+    char humidity_result[256] = "";
+    char gas_result[256] = "";
 
     while (1) {
         int result = recv(client_socket, buffer, BUF_SIZE, 0);
@@ -46,9 +58,8 @@ void *handle_client(void *arg) {
         }
 
         buffer[result] = '\0';
-        //printf("%s\n", buffer);
         char *auth_header = strstr(buffer, "Authorization: Basic ");
-        if (!auth_header || check_auth(auth_header)) {  
+        if (!auth_header || !check_auth(auth_header)) {  
             char response[1024] = "HTTP/1.1 401 Unauthorized\r\n"
                                   "WWW-Authenticate: Basic realm=\"User Visible Realm\"\r\n"
                                   "\r\n";
@@ -56,26 +67,14 @@ void *handle_client(void *arg) {
             break;
         }
 
-        fp = popen("sense -t -p -m -g", "r");
-        if (fp == NULL) {
-            perror("popen");
-            exit(EXIT_FAILURE);
-        }
-        // Считывание вывода команды в переменную
-        while (fgets(results, sizeof(results), fp) != NULL) {
-            printf("%s", results); // Для примера выводим результат на экран
-        }
-        pclose(fp);
-
-
-        if (strstr(buffer, "GET /on1") != NULL) {
-            system("echo 'LED #1 is ON'");
-        } else if (strstr(buffer, "GET /off1") != NULL) {
-            system("echo 'LED #1 is OFF'");
-        } else if (strstr(buffer, "GET /on2") != NULL) {
-            system("echo 'LED #2 is ON'");
-        } else if (strstr(buffer, "GET /off2") != NULL) {
-            system("echo 'LED #2 is OFF'");
+        if (strstr(buffer, "GET /temperature") != NULL) {
+            execute_command("sense -t", temp_result, sizeof(temp_result));
+        } else if (strstr(buffer, "GET /pressure") != NULL) {
+            execute_command("sense -p", pressure_result, sizeof(pressure_result));
+        } else if (strstr(buffer, "GET /humidity") != NULL) {
+            execute_command("sense -m", humidity_result, sizeof(humidity_result));
+        } else if (strstr(buffer, "GET /gas") != NULL) {
+            execute_command("sense -g", gas_result, sizeof(gas_result));
         }
 
         char response[BUF_SIZE];
@@ -88,25 +87,34 @@ void *handle_client(void *arg) {
                  "  <head>"
                  "    <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">"
                  "  </head>"
-                 "  <h1>ODROID - Web Server</h1>"
-                 "  <p>LED #1"
-                 "    <a href=\"on1\">"
-                 "      <button>ON</button>"
-                 "    </a>&nbsp;"
-                 "    <a href=\"off1\">"
-                 "      <button>OFF</button>"
-                 "    </a>"
-                 "  </p>"
-                 "  <p>LED #2"
-                 "    <a href=\"on2\">"
-                 "      <button>ON</button>"
-                 "    </a>&nbsp;"
-                 "    <a href=\"off2\">"
-                 "      <button>OFF</button>"
-                 "    </a>"
-                 "  </p>"
-                 "  <pre>%s</pre>"
-                 "</html>", results);
+                 "  <body>"
+                 "    <h1>ODROID - Web Server</h1>"
+                 "    <p>Температура"
+                 "      <a href=\"temperature\">"
+                 "        <button>Показать</button>"
+                 "      </a>"
+                 "    </p>"
+                 "    <pre>%s</pre>"
+                 "    <p>Давление"
+                 "      <a href=\"pressure\">"
+                 "        <button>Показать</button>"
+                 "      </a>"
+                 "    </p>"
+                 "    <pre>%s</pre>"
+                 "    <p>Влажность"
+                 "      <a href=\"humidity\">"
+                 "        <button>Показать</button>"
+                 "      </a>"
+                 "    </p>"
+                 "    <pre>%s</pre>"
+                 "    <p>Газ"
+                 "      <a href=\"gas\">"
+                 "        <button>Показать</button>"
+                 "      </a>"
+                 "    </p>"
+                 "    <pre>%s</pre>"
+                 "  </body>"
+                 "</html>", temp_result, pressure_result, humidity_result, gas_result);
 
         send(client_socket, response, strlen(response), 0);
     }
