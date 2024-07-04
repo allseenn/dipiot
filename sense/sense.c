d56fcf509097b8198cef47724097f56ed34ad15e
allseenn <allseen@live.ru>
2024-07-04 15:45:38 +0000

    gas fist

--- sense/sense.c
+++ sense/sense.c
@@ -53,23 +53,24 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-
-    gas_sensor.tph_sett.os_temp = BME680_OS_8X;
     gas_sensor.dev_id = BME680_I2C_ADDR_SECONDARY;
     gas_sensor.intf = BME680_I2C_INTF;
     gas_sensor.read = user_i2c_read;
     gas_sensor.write = user_i2c_write;
     gas_sensor.delay_ms = user_delay_ms;
     gas_sensor.amb_temp = 25;
+    gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
+    gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
+    gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
     gas_sensor.power_mode = BME680_FORCED_MODE;
     gas_sensor.tph_sett.os_hum = BME680_OS_2X;
     gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
     gas_sensor.tph_sett.os_pres = BME680_OS_4X;
+    gas_sensor.tph_sett.os_temp = BME680_OS_8X;
     /* Create a ramp heat waveform in 3 steps */
-    gas_sensor.gas_sett.run_gas = BME680_ENABLE_GAS_MEAS;
-    gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
-    gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
 
+    float hectoPascal = 0.750063755419211;
+    struct bme680_field_data data;
     int8_t rslt = BME680_OK;
     rslt = bme680_init(&gas_sensor);
     uint8_t set_required_settings;
@@ -78,11 +79,13 @@ int main(int argc, char const *argv[])
     rslt = bme680_set_sensor_mode(&gas_sensor);
     uint16_t meas_period;
     bme680_get_profile_dur(&meas_period, &gas_sensor);
-    struct bme680_field_data data;
-    float hectoPascal = 0.750063755419211;
     sleep(TIMEOUT);
     rslt = bme680_get_sensor_data(&data, &gas_sensor);
     for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "-g") == 0) {
+                printf("%f ", data.gas_resistance);
+
+            } 
             if (strcmp(argv[i], "-t") == 0) {
                 printf("%f ", data.temperature / 100.0f);
             }
@@ -95,10 +98,6 @@ int main(int argc, char const *argv[])
 
                 
             }
-            if (strcmp(argv[i], "-g") == 0) {
-                printf("%f ", data.gas_resistance);
-
-            } 
             if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "") == 0)  {
                 printf("Usage: sense [-t -p -h -g]\n");
                 printf("-t: Temperature degrees in Celsius\n");
