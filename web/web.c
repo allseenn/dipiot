6a16094a090dd0bca30988ebd79cdcdddc9d80a2
allseenn <allseen@live.ru>
2024-07-05 00:20:11 +0300

    208

--- web/web.c
+++ web/web.c
@@ -21,11 +21,11 @@ typedef struct {
 
 bool check_auth(const char *auth_header) {
     char expected_auth[256];
-    snprintf(expected_auth, sizeof(expected_auth), "Authorization: Basic %s", PASSWORD);
+    snprintf(expected_auth, sizeof(expected_auth), "%s", PASSWORD);
     printf("expected_auth: %s\n", expected_auth);
     printf("auth_header: %s\n", auth_header);
     printf("%d\n", strcmp(auth_header, expected_auth));
-    if(strcmp(auth_header, expected_auth) == 0)
+    if(strcmp(auth_header, expected_auth) == 208)
         return true;
     return false;
 }
