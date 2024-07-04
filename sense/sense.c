ed00a6faf8309b3f94565eb38047871604426599
allseenn <allseen@live.ru>
2024-07-04 14:08:15 +0000

    01

--- sense/sense.c
+++ sense/sense.c
@@ -58,24 +58,37 @@ int main(int argc, char const *argv[])
     gas_sensor.write = user_i2c_write;
     gas_sensor.delay_ms = user_delay_ms;
     gas_sensor.amb_temp = 25;
+    gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
+    gas_sensor.power_mode = BME680_FORCED_MODE;
 
 
     int8_t rslt = BME680_OK;
     rslt = bme680_init(&gas_sensor);
     uint8_t set_required_settings;
-    gas_sensor.tph_sett.filter = BME680_FILTER_SIZE_3;
+    set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL | BME680_GAS_SENSOR_SEL;
+    rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
+    rslt = bme680_set_sensor_mode(&gas_sensor);
+    /* Select the power mode */
+    /* Must be set before writing the sensor configuration */
+    uint16_t meas_period;
+    bme680_get_profile_dur(&meas_period, &gas_sensor);
+    struct bme680_field_data data;
+    rslt = bme680_get_sensor_data(&data, &gas_sensor);
 
     for (int i = 1; i < argc; i++) {
             if (strcmp(argv[i], "-t") == 0) {
                 gas_sensor.tph_sett.os_temp = BME680_OS_8X;
+                printf("T: %.2f" , data.temperature / 100.0f);
                 
             }
             if (strcmp(argv[i], "-p") == 0) {
                 gas_sensor.tph_sett.os_pres = BME680_OS_4X;
+                printf("P: %.2f hPa, H %.2f %%rH ", data.pressure / 100.0f);
                 
             }
             if (strcmp(argv[i], "-h") == 0) {
                 gas_sensor.tph_sett.os_hum = BME680_OS_2X;
+                printf("H %.2f %%rH ", data.humidity / 1000.0f);
                 
             }
             if (strcmp(argv[i], "-g") == 0) {
@@ -83,6 +96,7 @@ int main(int argc, char const *argv[])
                 /* Create a ramp heat waveform in 3 steps */
                 gas_sensor.gas_sett.heatr_temp = 320; /* degree Celsius */
                 gas_sensor.gas_sett.heatr_dur = 150; /* milliseconds */
+                printf(", G: %d ohms", data.gas_resistance);
                 
             } 
             if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "") == 0)  {
@@ -94,48 +108,7 @@ int main(int argc, char const *argv[])
                 return 0;
             }
         }
-
-
-    /* Select the power mode */
-    /* Must be set before writing the sensor configuration */
-    gas_sensor.power_mode = BME680_FORCED_MODE;
-
-    /* Set the required sensor settings needed */
-    set_required_settings = BME680_OST_SEL | BME680_OSP_SEL | BME680_OSH_SEL | BME680_FILTER_SEL | BME680_GAS_SENSOR_SEL;
-
-    /* Set the desired sensor configuration */
-    rslt = bme680_set_sensor_settings(set_required_settings,&gas_sensor);
-
-    /* Set the power mode */
-    rslt = bme680_set_sensor_mode(&gas_sensor);
-
-    /* Get the total measurement duration so as to sleep or wait till the
-    * measurement is complete */
-    uint16_t meas_period;
-    bme680_get_profile_dur(&meas_period, &gas_sensor);
-
-    struct bme680_field_data data;
-
-
-    while(1)
-    {
-        user_delay_ms(meas_period); /* Delay till the measurement is ready */
-
-            rslt = bme680_get_sensor_data(&data, &gas_sensor);
-
-            printf("T: %.2f degC, P: %.2f hPa, H %.2f %%rH ", data.temperature / 100.0f,
-                data.pressure / 100.0f, data.humidity / 1000.0f );
-            /* Avoid using measurements from an unstable heating setup */
-            if(data.status & BME680_GASM_VALID_MSK)
-                printf(", G: %d ohms", data.gas_resistance);
-
-            printf("\r\n");
-
-            /* Trigger the next measurement if you would like to read data out continuously */
-            if (gas_sensor.power_mode == BME680_FORCED_MODE) {
-                rslt = bme680_set_sensor_mode(&gas_sensor);
-            }
-    }
+    printf("\n");
     return 0;
 }
 
