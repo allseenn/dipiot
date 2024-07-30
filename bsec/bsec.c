4aff0545040ae26a62562366a3cf8dbe45e17c22
allseenn <allseen@live.ru>
2024-07-30 20:48:02 +0000

    60

--- bsec/bsec.c
+++ bsec/bsec.c
@@ -136,7 +136,8 @@ int8_t bus_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr,
 void _sleep(uint32_t t_ms)
 {
   struct timespec ts;
-  ts.tv_sec = t_ms / 1000;
+  //ts.tv_sec = t_ms / 1000;
+  ts.tv_sec = 60;
   /* mod because nsec must be in the range 0 to 999999999 */
   ts.tv_nsec = (t_ms % 1000) * 1000000L;
   nanosleep(&ts, NULL);
