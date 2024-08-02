35bdb55f1825b59f484bc05443ad8414c15f6600
allseen <allseen@live.ru>
2024-08-02 05:22:13 +0300

    path

--- bsec.sh
+++ bsec.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 mkfifo /tmp/bsec
 /usr/local/sbin/bsec > /tmp/bsec &
 cat /tmp/bsec | /usr/local/sbin/mqtt &
