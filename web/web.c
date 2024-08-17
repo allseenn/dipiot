771e11c1669e606c79fbc2a64116d6ec00e9facb
allseenn <allseen@live.ru>
2024-08-17 17:06:10 +0300

    close

--- web/web.c
+++ web/web.c
@@ -66,6 +66,7 @@ void *handle_client(void *arg) {
         snprintf(response, sizeof(response),
 "HTTP/1.1 200 OK\r\n"
 "Content-Type: text/html; charset=utf-8\r\n"
+"Connection: close\r\n" 
 "\r\n"
 "<!DOCTYPE HTML>"
 "<html>"
