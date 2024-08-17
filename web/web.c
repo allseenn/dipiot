e9b32d37b2e932fc8b80215298b14b90b0eecfc1
allseenn <allseen@live.ru>
2024-08-17 16:21:45 +0300

    ajax

--- web/web.c
+++ web/web.c
@@ -3,177 +3,147 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <stdbool.h>
 #include <pthread.h>
-#include <signal.h>
+#include <sys/socket.h>
 
 #define PORT 8080
-#define BUF_SIZE 1024
-
-typedef struct {
-    int client_socket;
-    struct sockaddr_in client_addr;
-} client_info;
 
 void *handle_client(void *arg) {
-    client_info *client = (client_info *)arg;
-    int client_socket = client->client_socket;
+    int client_socket = *(int *)arg;
+    free(arg);
 
-    while (1) {
-        FILE *fp;
-        char line[99];
+    char *response = "HTTP/1.1 200 OK\r\n"
+                     "Content-Type: text/html; charset=utf-8\r\n"
+                     "\r\n"
+                     "<!DOCTYPE HTML>"
+                     "<html>"
+                     "<head>"
+                     "<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">"
+                     "<script src=\"https://code.jquery.com/jquery-3.6.0.min.js\"></script>"
+                     "<script>"
+                     "function fetchData() {"
+                     "    $.get('/data', function(data) {"
+                     "        var json = JSON.parse(data);"
+                     "        $('#temp').text(json.temp);"
+                     "        $('#raw_temp').text(json.raw_temp);"
+                     "        $('#humidity').text(json.humidity);"
+                     "        $('#raw_hum').text(json.raw_hum);"
+                     "        $('#press').text(json.press);"
+                     "        $('#gas').text(json.gas);"
+                     "        $('#ceCO2').text(json.ceCO2);"
+                     "        $('#bVOC').text(json.bVOC);"
+                     "        $('#IAQ').text(json.IAQ);"
+                     "        $('#SIAQ').text(json.SIAQ);"
+                     "        $('#IAQ_ACC').text(json.IAQ_ACC);"
+                     "        $('#status').text(json.status);"
+                     "        $('#Dynamic_Rad').text(json.Dynamic_Rad);"
+                     "        $('#Static_Rad').text(json.Static_Rad);"
+                     "    });"
+                     "}"
+                     "setInterval(fetchData, 3000);"
+                     "</script>"
+                     "</head>"
+                     "<body>"
+                     "<h1>Данные датчиков</h1>"
+                     "<table border='1' id='data-table'>"
+                     "    <tr>"
+                     "        <th>Температура</th>"
+                     "        <th>Температура (сырая)</th>"
+                     "        <th>Влажность</th>"
+                     "        <th>Влажность (сырая)</th>"
+                     "        <th>Давление</th>"
+                     "        <th>Газ</th>"
+                     "        <th>СО2 эквивалент</th>"
+                     "        <th>Летучие органические вещества</th>"
+                     "        <th>IAQ</th>"
+                     "        <th>SIAQ</th>"
+                     "        <th>IAQ (скорректированный)</th>"
+                     "        <th>Статус</th>"
+                     "        <th>Динамическое излучение</th>"
+                     "        <th>Статистическое излучение</th>"
+                     "    </tr>"
+                     "    <tr>"
+                     "        <td id='temp'></td>"
+                     "        <td id='raw_temp'></td>"
+                     "        <td id='humidity'></td>"
+                     "        <td id='raw_hum'></td>"
+                     "        <td id='press'></td>"
+                     "        <td id='gas'></td>"
+                     "        <td id='ceCO2'></td>"
+                     "        <td id='bVOC'></td>"
+                     "        <td id='IAQ'></td>"
+                     "        <td id='SIAQ'></td>"
+                     "        <td id='IAQ_ACC'></td>"
+                     "        <td id='status'></td>"
+                     "        <td id='Dynamic_Rad'></td>"
+                     "        <td id='Static_Rad'></td>"
+                     "    </tr>"
+                     "</table>"
+                     "</body>"
+                     "</html>";
 
-        fp = fopen("/tmp/bsec", "r");
-        if (fp == NULL) {
-            printf("Error opening file /tmp/bsec\n");
-            break;
-        }
-        
-        if (fgets(line, sizeof(line), fp) == NULL) {
-            printf("Error reading from file /tmp/bsec\n");
-            fclose(fp);
-            break;
-        }
-
-        fclose(fp);
-
-        float arr[12] = {0};
-        int arr_i = 0;
-        char *token = strtok(line, " ");
-        while (token != NULL) {
-            arr[arr_i++] = atof(token);
-            token = strtok(NULL, " ");
-        }
-
-        int rad[2];
-        char cmd[100];
-        FILE *fp_rad;
-        char rad_line[50];
-
-        sprintf(cmd, "./rad.sh");
-        fp_rad = popen(cmd, "r");
-        if (fp_rad == NULL) {
-            printf("Failed to run command\n");
-            break;
-        }
-
-        while (fgets(rad_line, sizeof(rad_line), fp_rad) != NULL) {
-            sscanf(rad_line, "%d %d", &rad[0], &rad[1]);
-        }
-        pclose(fp_rad);
-
-        char response[BUF_SIZE];
-        snprintf(response, sizeof(response),
-"HTTP/1.1 200 OK\r\n"
-"Content-Type: text/html; charset=utf-8\r\n"
-"\r\n"
-"<!DOCTYPE HTML>"
-"<html>"
-"  <head>"
-"  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" http-equiv=\"refresh\" content=\"3\">"
-"  </head>"
-"  <h1>ODROID: WEB-MET</h1>"
-"  <table border=\"1\"><tr><th>temp</th>"
-"  <th>raw_temp</th>"
-"  <th>humidity</th>"
-"  <th>raw_hum</th>"
-"  <th>press</th>"
-"  <th>gas</th>"
-"  <th>ceCO2</th>"
-"  <th>bVOC</th>"
-"  <th>IAQ</th>"
-"  <th>SIAQ</th>"
-"  <th>IAQ_ACC</th>"
-"  <th>status</th>"
-"  <th>Dynamic Rad</th>"
-"  <th>Static Rad</th></tr>"
-"  <tr><td>%.1f</td><td>%.1f</td><td>%.1f</td><td>%.1f</td>"
-"  <td>%.0f</td><td>%.0f</td><td>%.0f</td><td>%.2f</td>"
-"  <td>%.0f</td><td>%.0f</td><td>%.0f</td><td>%.0f</td>"
-"  <td>%d</td><td>%d</td></tr>"
-"  <tr><td>C&deg;</td><td>C&deg;</td><td>&percnt;</td><td>&percnt;</td>"
-"  <td>mmHg</td><td>KOM</td><td>ppm</td><td>ppm</td>"
-"  <td>index</td><td>index</td><td>num</td><td>num</td>"
-"  <td>&mu;R/h</td><td>&mu;R/h</td></tr></table>"
-"</html>", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9], arr[10], arr[11], rad[0], rad[1]);
-        
-        send(client_socket, response, strlen(response), 0);
-        sleep(3);
-    }
-
+    // Отправка ответа клиенту 
+    send(client_socket, response, strlen(response), 0);
+    
     close(client_socket);
-    free(client);
-    pthread_exit(NULL);
-}
-
-volatile sig_atomic_t server_running = 1;
-int server_socket;
-
-void handle_signal(int sig) {
-    if (sig == SIGINT) {
-        printf("\nReceived SIGINT. Shutting down server...\n");
-        server_running = 0;
-        close(server_socket);
-    }
+    return NULL;
 }
 
 int main() {
-    struct sockaddr_in server_addr;
-    socklen_t size;
+    int server_socket, *new_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
-        perror("Error establishing socket");
+        perror("Ошибка при создании сокета");
         exit(EXIT_FAILURE);
     }
-
-    printf("=> Socket server has been created...\n");
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Error binding connection");
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Ошибка при привязке сокета");
+        close(server_socket);
         exit(EXIT_FAILURE);
     }
 
-    size = sizeof(server_addr);
-    printf("=> Looking for clients...\n");
-
-    listen(server_socket, 5);
-
-    signal(SIGINT, handle_signal);
-
-    pthread_t tid;
-    while (server_running) {
-        client_info *client = malloc(sizeof(client_info));
-        client->client_socket = accept(server_socket, (struct sockaddr*)&client->client_addr, &size);
-
-        if (client->client_socket < 0) {
-            if (!server_running) {
-                free(client);
-                break;
-            }
-            perror("Error on accepting");
-            free(client);
-            continue;
-        }
-
-        printf("=> Connected with the client, you are good to go...\n");
-
-        if (pthread_create(&tid, NULL, handle_client, (void *)client) != 0) {
-            perror("Could not create thread");
-            free(client);
-            continue;
-        }
-
-        pthread_detach(tid);
+    if (listen(server_socket, 5) < 0) {
+        perror("Ошибка при прослушивании");
+        close(server_socket);
+        exit(EXIT_FAILURE);
     }
 
-    printf("Closing server socket...\n");
-    close(server_socket);
-    printf("Goodbye...\n");
+    printf("Сервер запущен на порту %d\n", PORT);
 
+    while (1) {
+        int *new_sock = malloc(sizeof(int));
+        if (new_sock == NULL) {
+            perror("Ошибка при выделении памяти");
+            continue;
+        }
+
+        *new_sock = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+        if (*new_sock < 0) {
+            perror("Ошибка при принятии подключения");
+            free(new_sock);
+            continue;
+        }
+
+        pthread_t client_thread;
+        if (pthread_create(&client_thread, NULL, handle_client, (void *)new_sock) != 0) {
+            perror("Ошибка при создании потока");
+            close(*new_sock);
+            free(new_sock);
+            continue;
+        }
+
+        pthread_detach(client_thread); // Отсоединяем поток для автоматического освобождения ресурсов
+    }
+
+    close(server_socket);
     return 0;
 }
+
